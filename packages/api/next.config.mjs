b2cdd7386fe6e12b481d0c77084c1b823861b462
Yvan2xEro <yvankanaj@gmail.com>
2026-03-11 16:27:58 +0100

    chore: webpack->turbopack

--- packages/api/next.config.mjs
+++ packages/api/next.config.mjs
@@ -3,14 +3,8 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  webpack: (webpackConfig) => {
-    webpackConfig.resolve.extensionAlias = {
-      '.cjs': ['.cts', '.cjs'],
-      '.js': ['.ts', '.tsx', '.js', '.jsx'],
-      '.mjs': ['.mts', '.mjs'],
-    }
-
-    return webpackConfig
+  turbopack: {
+    root: process.env.TURBOPACK_ROOT || '../..',
   },
 }
 
