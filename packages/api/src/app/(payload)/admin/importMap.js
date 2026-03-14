b0a41fc2c4b7cc5af23b3ffb77e9bf21b2634fd6
Yvan2xEro <yvankanaj@gmail.com>
2026-03-14 02:36:37 +0100

    fix: importmaps

--- packages/api/src/app/(payload)/admin/importMap.js
+++ packages/api/src/app/(payload)/admin/importMap.js
@@ -1,9 +1,24 @@
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from "@payloadcms/next/rsc";
 import { default as default_1a7510af427896d367a49dbf838d2de6 } from "@/components/BeforeDashboard";
+import { default as default_09b819edf1a6f37d51b139d5bc6413f5 } from "@/components/nav/ModerationNav";
+import { default as default_084c38875d0c01ac5cda002d0336b4c8 } from "@/components/widgets/ModerationWidget";
+import { default as default_2388bd5dd6b00dd4a1f3fa404f240977 } from "../../../components/views/ModerationQueue";
+import { default as default_ca7a4afb0c16588850c59fd9ecee0e82 } from "../../../components/views/ReportsQueue";
+import { default as default_69f19aeee1cacfe9a54bb226875b385d } from "../../../components/views/UserManagement";
 
 export const importMap = {
+	"@/components/nav/ModerationNav#default":
+		default_09b819edf1a6f37d51b139d5bc6413f5,
 	"@/components/BeforeDashboard#default":
 		default_1a7510af427896d367a49dbf838d2de6,
+	"@/components/widgets/ModerationWidget#default":
+		default_084c38875d0c01ac5cda002d0336b4c8,
+	"/components/views/ModerationQueue#default":
+		default_2388bd5dd6b00dd4a1f3fa404f240977,
+	"/components/views/ReportsQueue#default":
+		default_ca7a4afb0c16588850c59fd9ecee0e82,
+	"/components/views/UserManagement#default":
+		default_69f19aeee1cacfe9a54bb226875b385d,
 	"@payloadcms/next/rsc#CollectionCards":
 		CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1,
 };
