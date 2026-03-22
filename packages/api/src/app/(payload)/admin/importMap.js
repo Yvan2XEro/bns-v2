a29f6160ab18e0828d51d017959f4d79a3c27f3a
Yvan2xEro <yvankanaj@gmail.com>
2026-03-22 12:00:25 +0100

    chore,feat: updating assets, eas update

--- packages/api/src/app/(payload)/admin/importMap.js
+++ packages/api/src/app/(payload)/admin/importMap.js
@@ -1,6 +1,10 @@
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from "@payloadcms/next/rsc";
 import { default as default_032993581af5259da0d9363de3b39378 } from "@/components/auth/AdminSocialLogin";
 import { default as default_1a7510af427896d367a49dbf838d2de6 } from "@/components/BeforeDashboard";
+import {
+	Icon as Icon_aa1ccffe8e7691b68760d0edc9b149c9,
+	Logo as Logo_aa1ccffe8e7691b68760d0edc9b149c9,
+} from "@/components/branding/AdminLogo";
 import { default as default_09b819edf1a6f37d51b139d5bc6413f5 } from "@/components/nav/ModerationNav";
 import { default as default_e0da21ab58da76cc2b239852738a910d } from "@/components/views/ModerationQueue";
 import { default as default_8faa6da8bc58c6e839b4e75ebee319f7 } from "@/components/views/ReportsQueue";
@@ -8,6 +12,8 @@ import { default as default_82ba70094ba21cc228ff4105fb671eee } from "@/component
 import { default as default_084c38875d0c01ac5cda002d0336b4c8 } from "@/components/widgets/ModerationWidget";
 
 export const importMap = {
+	"@/components/branding/AdminLogo#Icon": Icon_aa1ccffe8e7691b68760d0edc9b149c9,
+	"@/components/branding/AdminLogo#Logo": Logo_aa1ccffe8e7691b68760d0edc9b149c9,
 	"@/components/nav/ModerationNav#default":
 		default_09b819edf1a6f37d51b139d5bc6413f5,
 	"@/components/BeforeDashboard#default":
